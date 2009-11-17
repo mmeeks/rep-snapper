3f8c7037dad8979061291926e462531bcc19c71a
repsnapper <repsnapper@686d4eb6-aa70-4f30-b8fa-ee57029aed71>
2009-11-17 13:09:50 +0000

    git-svn-id: http://svn.kulitorum.com/RepSnapper/Src@14 686d4eb6-aa70-4f30-b8fa-ee57029aed71

---
+++ search.h
@@ -0,0 +1,32 @@
+#include <vmmlib/vmmlib.h>
+#include <vector>
+
+#include <ANN/ANN.h>
+
+using namespace std;					// make std:: accessible
+using namespace vmml;
+
+
+class Search{
+public:
+	Search(){dim = 3; nnIdx = 0; dists = 0; kdTree = 0; readyToBeSearched = false;}
+	~Search();
+	
+	bool addPoint(const Vector3f &p);			// returns 1=OK, 0=bad
+	bool Init();								// Make ready - call after adding points
+	int search(const Vector3f &p);				// returns index of nearest point or -1 for error
+
+private:
+	bool readyToBeSearched;
+	std::vector<Vector3f> points;				// temp buffer for points
+	int					dim;					// Dimension to work in, usually 2 or 3
+	int					MaxPointCount;			// Max n_points
+	int					nPts;					// actual number of data points
+	ANNpointArray		dataPts;				// data points
+	ANNpoint			queryPt;				// query point
+	ANNidxArray			nnIdx;					// near neighbor indices
+	ANNdistArray		dists;					// near neighbor distances
+	ANNkd_tree*			kdTree;					// search structure
+};
+
+
