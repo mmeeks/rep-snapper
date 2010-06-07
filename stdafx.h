2844f48d78947297dc3ec3fa38c41bcfe64f30f1
Michael Meeks <michael.meeks@novell.com>
2010-06-07 12:36:12 +0100

    remove un-needed search.h include

--- stdafx.h
+++ stdafx.h
@@ -43,7 +43,6 @@ typedef unsigned int        UINT;
 #include "ModelViewController.h"
 #include "Printer.h"
 #include "ProcessController.h"
-#include "search.h"
 #include "stl.h"
 #include "UI.h"
 #include "xml/XML.H"
